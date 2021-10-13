6b47f25d60a2f5db1eb7f9161eeba3baa587aeed
Staffan Westerlund <steif.98@gmail.com>
2021-10-13 13:25:19 +0200

    changed model

--- models/result-model.js
+++ models/result-model.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Result = new Schema(
-    {
-        alias: {type: String, required: true},
-        age: {type: Number, required: true},
-        gender: {type: String, required: true},
-        result: {type: Array, required: true},
-        answers: {type: Array, required: true},
+const Result = new Schema({
+  alias: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  vision: { type: String, required: true },
+  computer: { type: Number, required: true },
+  experience: { type: Number, required: true },
+  result: { type: Array, required: true },
+  answers: { type: Array, required: true },
+});
 
-    }
-);
-
-module.exports = mongoose.model('results', Result)
+module.exports = mongoose.model('results', Result);
